fa6e1db00b28bed8aca2f1bd26cd698b9e025831
idleCat90 <s.a.revin@yandex.ru>
2023-11-27 13:33:01 +0300

    refactor: simplify logic, add @UtilityClass

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,47 +1,21 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.stereotype.Component;
-import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.model.User;
+import lombok.experimental.UtilityClass;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Component
+@UtilityClass
 public class UserMapper {
-    public User toUser(UserDto userDto) {
-        return User.builder()
-                .id(null)
-                .name(userDto.getName())
-                .email(userDto.getEmail())
-                .build();
-    }
 
     public UserDto toUserDto(User user) {
-        return UserDto.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .email(user.getEmail())
-                .build();
+        return new UserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail());
     }
 
-    public List<UserDto> toUserDtoList(List<User> userList) {
-        List<UserDto> userDtoList = new ArrayList<>();
-        for (User user : userList) {
-            userDtoList.add(toUserDto(user));
-        }
-        return userDtoList;
-    }
-
-    public void updateUserByDto(UserDto userDto, User user) {
-        String name = userDto.getName();
-        if (name != null) {
-            user.setName(name);
-        }
-
-        String email = userDto.getEmail();
-        if (email != null) {
-            user.setEmail(email);
-        }
+    public User toUser(UserDto userDto) {
+        return new User(
+                userDto.getId(),
+                userDto.getName(),
+                userDto.getEmail());
     }
 }
