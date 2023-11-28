1474016b7294b7a74a3b73663df7aa801c102598
idleCat90 <s.a.revin@yandex.ru>
2023-11-28 13:24:47 +0300

    refactor: implement bookings and comments

---
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.item.service;
+
+import ru.practicum.shareit.item.dto.CommentReqDto;
+import ru.practicum.shareit.item.dto.CommentRespDto;
+import ru.practicum.shareit.item.dto.ItemReqDto;
+import ru.practicum.shareit.item.dto.ItemRespDto;
+
+import java.util.List;
+
+public interface ItemService {
+    ItemRespDto add(Long userId, ItemReqDto itemReqDto);
+
+    ItemRespDto update(Long userId, Long itemId, ItemReqDto itemReqDto);
+
+    ItemRespDto findById(Long userId, Long itemId);
+
+    List<ItemRespDto> findAll(Long userId);
+
+    List<ItemRespDto> search(Long userId, String text);
+
+    CommentRespDto addComment(Long userId, CommentReqDto commentReqDto, Long itemId);
+}
